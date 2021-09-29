f949dd4dda789aac6195dceb1cc713b261f4c58e
lgcaobianco <gu.caobianco@gmail.com>
2021-09-28 22:11:26 -0300

    created validation for todo with empty names

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,13 +1,31 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+} from 'aws-lambda'
 import 'source-map-support/register'
 import * as middy from 'middy'
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { parseUserId } from '../../auth/utils'
-import { save } from '../../repository/todo-repository'
+import {
+  CreateTodoRequest
+} from '../../requests/CreateTodoRequest'
+import {
+  parseUserId
+} from '../../auth/utils'
+import {
+  save
+} from '../../repository/todo-repository'
 
 export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  async (event: APIGatewayProxyEvent): Promise < APIGatewayProxyResult > => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    if (newTodo.name == null || newTodo.name.trim().length < 1) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          field: 'name',
+          error: 'cant save empty todo'
+        })
+      };
+    }
     const userId = parseUserId(event.headers.Authorization.split(" ")[1]);
     const todo = await save(newTodo, userId);
     console.log("todo created:" + JSON.stringify(todo));
@@ -22,4 +40,4 @@ export const handler = middy(
         item: todo
       })
     };
-});
+  });
